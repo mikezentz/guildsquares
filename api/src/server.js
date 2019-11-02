a1db26d4bbac0f74e8aa5f77298c625b6aa17a82
Mike Zentz <mzentz@gmail.com>
2019-11-01 19:21:38 -0700

    test

--- api/src/server.js
+++ api/src/server.js
@@ -1,36 +1,84 @@
-const WebSocket = require("ws")
-const wss = new WebSocket.Server({port: 5050})
-const axios = require("axios")
-const ip = require("ip")
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({port: 5050});
+const axios = require("axios");
+const ip = require("ip");
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { AsyncRouter } = require("express-async-router")
 
 // const response = await axios.get("https://jsonplaceholder.typicode.com/posts")
-const ipAddress = ip.address()
 
-const subnet = ipAddress.split(".").slice(0,3).join(".") + "."
-const liveServers = []
+let liveservers = [];
 
-const main = async () => {
+const getServers = async () => {
+    while(true){
+        let buildservers = []
+        const ipAddress = ip.address();
+        const subnet =
+            ipAddress
+                .split(".")
+                .slice(0, 3)
+                .join(".") + ".";
 
-    for (i = 254; i > 1; i--) {
+        for (i = 1; i < 254; i++) {
+            const scannedServer = "http://" + subnet + i.toString();
+            try {
+                const response = await axios.get(scannedServer + ":3000", {
+                    timeout: 100
+                });
 
-        const scannedServer = "http://" + subnet + i.toString() + ":8000"
-        console.log(scannedServer)
-        try {
-            const response = await axios.get(scannedServer)
+                if (response.status == "200") {
+                    console.log(scannedServer + ":3000");
+                    buildservers.push(scannedServer + ":3000");
+                }
+            } catch (e) {}
+            try {
+                const response = await axios.get(scannedServer + ":8000", {
+                    timeout: 100
+                });
 
-            if(response.status=="200"){
-                console.log(response.data)
-                liveservers.push(scannedServer)
+                if (response.status == "200") {
+                    console.log(scannedServer + ":8000");
+                    buildservers.push(scannedServer + ":8000");
+                }
+            } catch (e) {}
+            try {
+                const response = await axios.get(scannedServer + ":8080", {
+                    timeout: 100
+                });
 
-            }
-        } catch (e) {
-
+                if (response.status == "200") {
+                    console.log(scannedServer + ":8080");
+                    buildservers.push(scannedServer + ":8080");
+                }
+            } catch (e) {}
         }
-
-
-
+        console.log(buildservers);
+        console.log("Done");
+        liveservers = buildservers
     }
-    console.log(liveservers)
+
+};
+
+const app = express()
+
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan("tiny"))
+
+app.get('/', (req, res) => {
+    res.send({liveservers})
+})
+
+const startServer = async (port = 1111, hostname = "localhost") => {
+    app.listen(port, hostname, () => {
+        console.log(`Listening at ${hostname}:${port}...`)
+    })
+
+
 }
 
-main()
+startServer()
+getServers()
